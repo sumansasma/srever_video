308f03409e03aab8c2d3cc80b622b1b34439553f
Suman <145822623+sumansasma@users.noreply.github.com>
2023-10-06 18:32:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.post('/upload', upload.single('video'), (req, res) => {
 
 // Define a route for fetching videos
 app.get('/fetch-videos', (req, res) => {
-    db.all('SELECT * FROM videos WHERE isDeleted = 0', (err, videos) => { // Only select videos where isDeleted is false
+    db.all('SELECT * FROM videos', (err, videos) => {
         if (err) {
             console.error(err.message);
             res.status(500).send('Error fetching videos.');
@@ -99,6 +99,7 @@ app.get('/fetch-videos', (req, res) => {
         }
     });
 });
+
 
 // Serve video files from a specific URL path
 app.get('/uploads/:videourl', (req, res) => {
